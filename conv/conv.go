62cf6d14891389480554963e5ebd831886a0d236
Chamundsen <simencamu@hotmail.com>
2023-02-11 13:13:38 +0100

    implementert konverteringsfunksjonene

--- conv/conv.go
+++ conv/conv.go
@@ -15,8 +15,26 @@ func FarhenheitToCelsius(value float64) float64 {
 	// Du skal ikke formattere float64 i denne funksjonen
 	// Gjør formattering i main.go med fmt.Printf eller
 	// lignende
-	return 56.67
+	return (value - 32) * (5.0 / 9.0)
 }
 
 // De andre konverteringsfunksjonene implementere her
-// ...
+func CelsiusToFahrenheit(value float64) float64 {
+	return (value * 9.0 / 5.0) + 32
+}
+
+func CelsiustoKelvin(value float64) float64 {
+	return (value + 273.15)
+}
+
+func KelvintoCelsius(value float64) float64 {
+	return (value - 273.15)
+}
+
+func FahrenheittoKelvin(value float64) float64 {
+	return (value-32)*(5.0/9.0) + 273.15
+}
+
+func KelvintoFahrenheit(value float64) float64 {
+	return (value-273.15)*(9.0/5.0) + 32
+}
