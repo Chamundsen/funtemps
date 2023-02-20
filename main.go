2025a21913e7612fed7fb393dd8f77c51bd5e9c5
Chamundsen <simencamu@hotmail.com>
2023-02-20 11:10:57 +0100

    Implementert flag

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/Chamundsen/funtemps/conv"
 )
 
-var fahrenheit float64
+var fahr float64
+var cel float64
+var kel float64
 var out string
 var funfacts string
 
-func main() {
+func printTemperatures() {
 	// ...
 	fahrenheitFloat := 134.0
 	celsius := conv.FarhenheitToCelsius(fahrenheitFloat)
@@ -36,4 +39,62 @@ func main() {
 	fahrenheit1 := conv.KelvinToFahrenheit(fahrenheitFloat1)
 	fmt.Printf("%.2f °F\n", fahrenheit1)
 
+}
+
+func init() {
+	/*
+	   Her er eksempler på hvordan man implementerer parsing av flagg.
+	   For eksempel, kommando
+	       funtemps -F 0 -out C
+	   skal returnere output: 0°F er -17.78°C
+	*/
+
+	// Definerer og initialiserer flagg-variablene
+	flag.Float64Var(&fahr, "F", 0.0, "temperatur i grader fahrenheit")
+	flag.Float64Var(&cel, "C", 0.0, "temperatur i grader celsius")
+	flag.Float64Var(&kel, "K", 0.0, "temperatur i grader kelvin")
+	// Du må selv definere flag-variablene for "C" og "K".
+	flag.StringVar(&out, "out", "0.0", "beregne temperatur i C - Celsius, F - Fahrenheit, K - Kelvin")
+	flag.StringVar(&funfacts, "funfacts", "sun", "\"funfacts\" om sun - solen, luna - Månen og terra - Jorden")
+	// Du må selv definere flag-variabelen for -t-flagget, som bestemmer
+	// hvilken temperaturskala du skal bruke når du viser funfacts
+
+}
+
+func main() {
+	flag.Parse()
+	printTemperatures()
+
+	if isFlagPassed("F") && out == "C" {
+		celsius := conv.FarhenheitToCelsius(fahr)
+		fmt.Printf("F: %.2f til C: %.2f\n", fahr, celsius)
+	} else if isFlagPassed("F") && out == "K" {
+		kelvin := conv.FahrenheitToKelvin(fahr)
+		fmt.Printf("F: %.2f til K: %.2f\n", fahr, kelvin)
+	} else if isFlagPassed("C") && out == "F" {
+		fahrenheit := conv.CelsiusToFahrenheit(cel)
+		fmt.Printf("C: %.2f til F: %.2f\n", cel, fahrenheit)
+	} else if isFlagPassed("C") && out == "K" {
+		kelvin := conv.CelsiusToFahrenheit(cel)
+		fmt.Printf("C: %.2f til K: %.2f\n", cel, kelvin)
+	} else if isFlagPassed("K") && out == "C" {
+		celsius := conv.CelsiusToFahrenheit(kel)
+		fmt.Printf("K: %.2f til C: %.2f\n", kel, celsius)
+	} else if isFlagPassed("K") && out == "F" {
+		fahrenheit := conv.CelsiusToFahrenheit(kel)
+		fmt.Printf("K: %.2f til F: %.2f\n", kel, fahrenheit)
+	} else {
+		fmt.Println("Noe gikk galt")
+	}
+
+}
+
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
 }
